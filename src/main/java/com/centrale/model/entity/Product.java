8e1110e3064266b3bedf1b08f66934b3b046b64d
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-10-17 16:44:56 +0100

    fix the product model

--- src/main/java/com/centrale/model/entity/Product.java
+++ src/main/java/com/centrale/model/entity/Product.java
@@ -1,23 +1,42 @@
 package com.centrale.model.entity;
 
-import javax.persistence.*;
 import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "products")
 public class Product {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @NotBlank
+    @Size(max = 100)
+    @Column(unique = true, nullable = false)
     private String name;
 
+    @Column
     private String description;
 
+    @NotNull
+    @DecimalMin("0.01")
     @Column(nullable = false)
     private BigDecimal price;
 
+    @NotNull
+    @Min(0)
     @Column(nullable = false)
     private Integer stock;
 
